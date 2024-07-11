9486f2f38e26a6c0106e519b7b21949cb16a02ec
Bruna-Letícia12 <brunaldsant@gmail.com>
2024-07-11 13:58:35 -0300

    Correcao de logicas + Inclusão de exceptions

--- src/main/java/com/ecommerce/project/ecommerce/enums/SaleStatus.java
+++ src/main/java/com/ecommerce/project/ecommerce/enums/SaleStatus.java
@@ -1,7 +1,26 @@
 package com.ecommerce.project.ecommerce.enums;
 
 public enum SaleStatus {
-    PROCESSANDO,
-    CONFIRMADA,
-    CANCELADA;
+    SHIPPED (1),
+    CONFIRMED (2),
+    CANCELED (3);
+
+    private int code;
+
+    private SaleStatus(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static SaleStatus valueOf(int code) {
+        for (SaleStatus status : SaleStatus.values()) {
+            if (status.getCode() == code) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Código Inválido");
+    }
 }
--- src/main/java/com/ecommerce/project/ecommerce/repositories/SaleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.project.ecommerce.repositories;
-
-import com.ecommerce.project.ecommerce.entities.SaleEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SaleRepository extends JpaRepository<SaleEntity, Integer> {
-}
