7aa4948ca3e12499b830b77447eee4da96d4b355
Bruna-Letícia12 <brunaldsant@gmail.com>
2024-07-11 13:58:11 -0300

    Atualizao de logicas das repositories

--- src/main/java/com/ecommerce/project/ecommerce/repositories/SaleItemRepository.java
+++ src/main/java/com/ecommerce/project/ecommerce/repositories/SaleItemRepository.java
@@ -1,9 +1,9 @@
 package com.ecommerce.project.ecommerce.repositories;
 
-import com.ecommerce.project.ecommerce.entities.SaleItemEntity;
+import com.ecommerce.project.ecommerce.entities.SaleItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SaleItemRepository extends JpaRepository<SaleItemEntity, Integer> {
+public interface SaleItemRepository extends JpaRepository<SaleItem, Long> {
 }
--- src/main/java/com/ecommerce/project/ecommerce/repositories/UserRepository.java
+++ src/main/java/com/ecommerce/project/ecommerce/repositories/UserRepository.java
@@ -1,9 +1,10 @@
 package com.ecommerce.project.ecommerce.repositories;
 
-import com.ecommerce.project.ecommerce.entities.Order;
+import com.ecommerce.project.ecommerce.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository  extends JpaRepository<Order, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
