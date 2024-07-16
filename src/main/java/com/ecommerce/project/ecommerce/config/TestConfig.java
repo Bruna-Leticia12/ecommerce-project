5d39054f8edd3774c06274962e736514d4e72eca
Bruna-Letícia12 <brunaldsant@gmail.com>
2024-07-16 16:16:44 -0300

    Atualizacao Instant

--- src/main/java/com/ecommerce/project/ecommerce/config/TestConfig.java
+++ src/main/java/com/ecommerce/project/ecommerce/config/TestConfig.java
@@ -1,12 +1,15 @@
 package com.ecommerce.project.ecommerce.config;
 
+import com.ecommerce.project.ecommerce.entities.Sale;
 import com.ecommerce.project.ecommerce.entities.User;
+import com.ecommerce.project.ecommerce.repositories.SaleRepository;
 import com.ecommerce.project.ecommerce.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
@@ -16,8 +19,8 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
-
-//    private SaleRepository saleRepository;
+    @Autowired
+    private SaleRepository saleRepository;
 
 
 //    public TestConfig() {}
@@ -42,6 +45,12 @@ public class TestConfig implements CommandLineRunner {
         User u2 = new User(null, "Renato Ferreira", "renato@gmail.com", "993268485", "852617");
 
         userRepository.saveAll(Arrays.asList(u1, u2));
+
+        Sale s1 = new Sale(null, Instant.parse("2024-07-16T10:53:07Z"), u1);
+        Sale s2 = new Sale(null, Instant.parse("2024-07-16T03:42:10Z"), u2);
+        Sale s3 = new Sale(null, Instant.parse("2024-07-16T15:21:22Z"), u1);
+
+        saleRepository.saveAll(Arrays.asList(s1, s2, s3));
     }
 
 }
